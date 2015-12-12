25498e99e6a4afa6473a18d2c3211427d89b8285
YOUR NAME <ashley_machado@outlook.com>
2015-12-12 12:37:59 -0500

    First Commit

---
+++ Exploratory Data/plot1.R
@@ -0,0 +1,17 @@
+## Working directory should be set at the location of zip file - exdata-data-household_power_consumption.zip
+
+unzip(zipfile = "exdata-data-household_power_consumption.zip")
+fileName <- "household_power_consumption.txt"
+
+##Below command will load file data with Date range 1/2/2007 and 2/2/2007.
+##Alternate command(not shown below) is to read all the data and then subset
+powerConc <- read.table(text = grep("^[1,2]/2/2007", readLines(fileName),value = T), sep = ";", header = T,
+       col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity"
+      ,"Sub_metering_1","Sub_metering_2","Sub_metering_3"), na.strings = "?")
+
+
+##Plot1
+hist(x = powerConc$Global_active_power, xlab = "Global Active Power(kilowatts)", col = "red", 
+     main = "Global Active Power")
+dev.copy(png, "plot1.png")
+dev.off()
